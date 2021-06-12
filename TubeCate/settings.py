6abcbb357e53361971add30d63d726f48b1d6ecb
VickramMS <mowlisri123@gmail.com>
2021-06-12 11:19:16 +0530

    Static

--- TubeCate/settings.py
+++ TubeCate/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.static',
+    'django.contrib.staticfiles',
     'rest_framework.authtoken'
    
     
